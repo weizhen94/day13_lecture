84c721853fa21b346f02d12e97c5ca02b2586bfc
weizhen94 <weizhen94@gmail.com>
2023-02-08 09:54:38 +0800

    first commit

---
+++ src/test/java/ibf2022/ssf/day13_lecture/Day13LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2022.ssf.day13_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day13LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
